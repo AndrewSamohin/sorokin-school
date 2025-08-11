514eee9c15b3fede2b7f6d7de1d84d0a49417c98
Andrew Samohin <andreusamokhin@mail.ru>
2025-08-11 18:02:49 +0300

    Внесение правок в ревью Basics

--- src/main/java/org/schoolsorokin/javacore/basics/ContactAccountingSystem.java
+++ src/main/java/org/schoolsorokin/javacore/basics/ContactAccountingSystem.java
@@ -3,10 +3,8 @@ package org.schoolsorokin.javacore.basics;
 import java.util.Scanner;
 
 public class ContactAccountingSystem {
-    int menuItem;
-    int index = 0;
-    int i = 0;
-    boolean flag = false;
+    private int menuItem;
+    private int index = 0;
 
     String[] names = new String[100];
     String[] phoneNumbers = new String[100];
@@ -15,91 +13,42 @@ public class ContactAccountingSystem {
     //Основной метод с циклом
     public void accountSystem() {
         do {
-            textMenu();
+            //Вывод меню
+            startMenu();
             System.out.println("Выберите пункт в меню: ");
             menuItem = sc.nextInt();
             sc.nextLine();
-            menuImplementation();
-
+            //Проверка на корректный ввод меню
+            if (menuItem < 5 &&  menuItem > 0) {
+                menuImplementation();
+            } else  {
+                System.out.println("Неизвестный пункт меню.");
+            }
         } while (menuItem != 5);
     }
 
     //Метод с реализацией пунктов в меню
-    public void menuImplementation() {
+    private void menuImplementation() {
         switch (menuItem) {
 
             //Добавления нового контакта
             case 1:
-                System.out.println("Напишите свое Имя: ");
-                String name = sc.nextLine();
-
-                if (!name.isBlank()) {
-                    System.out.println("Напишите свой телефон: ");
-                    String phoneNumber = sc.nextLine();
-
-                    names[index] = name;
-                    phoneNumbers[index] = phoneNumber;
-                    index++;
-                } else {
-                    System.out.println("Имя не может быть пустым");
-                }
+                addContact();
                 break;
 
             //Вывод всех контактов
             case 2:
-                int a = 1;
-
-                for (i = 0; i < names.length; i++, a++) {
-                    if (names[i] != null &&  phoneNumbers[i] != null) {
-                        System.out.println(a + ". " + names[i] + " - " + phoneNumbers[i]);
-                        flag = true;
-                    }
-                }
-                if (!flag) {
-                    System.out.println("Контакты закончились.");
-                }
-
+                displayAllContacts();
                 break;
 
             //Поиск контакта по имени
             case 3:
-                System.out.println("Имя для поиска: ");
-                String enteredName = sc.nextLine();
-
-                for (i = 0; i < names.length; i++) {
-                    if (names[i] != null && names[i].equalsIgnoreCase(enteredName)) {
-                        System.out.println("Номер телефона " + enteredName + ": " + phoneNumbers[i]);
-                        flag = true;
-                        break;
-                    }
-                }
-                if(!flag) {
-                    System.out.println("Контакт с именем " + enteredName + " не найден.");
-                }
+                lookForContact();
                 break;
 
             //Удаление контакта по имени
             case 4:
-                System.out.println("Введите имя для удаления контакта: ");
-                String nameToDelete = sc.nextLine();
-
-                for (int i = 0; i < index; i++) {
-                    if (names[i].equalsIgnoreCase(nameToDelete)) {
-                        for (int j = i; j <  index - 1; j++) {
-                            names[j] = names[j + 1];
-                            phoneNumbers[j] = phoneNumbers[j + 1];
-                        }
-                        names[index - 1] = null;
-                        phoneNumbers[index - 1] = null;
-                        index--;
-                        flag = true;
-                        System.out.println("Контакт " + nameToDelete + " успешно удален.");
-                        break;
-                    }
-                }
-                if (!flag) {
-                    System.out.println("Контакт с именем " + nameToDelete + " не найден.");
-                }
+                deleteContact();
                 break;
 
             //Выход из программы
@@ -107,13 +56,83 @@ public class ContactAccountingSystem {
                 break;
 
             default:
-                System.out.println("Неизвестный пункт меню.");
                 break;
         }
     }
 
+    //Добавление нового контакта
+    private void addContact() {
+        System.out.println("Напишите имя: ");
+        String name = sc.nextLine();
+
+        if (!name.isBlank()) {
+            System.out.println("Напишите телефон: ");
+            String phoneNumber = sc.nextLine();
+            if (phoneNumber.isBlank()) {
+                System.out.println("Номер не может быть пустым.");
+                return;
+            }
+            names[index] = name;
+            phoneNumbers[index] = phoneNumber;
+            index++;
+        } else {
+            System.out.println("Имя не может быть пустым");
+        }
+    }
+
+    //Вывод всех контактов
+    private void displayAllContacts() {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] != null &&  phoneNumbers[i] != null) {
+                System.out.println((i + 1)
+                        + ". " + names[i]
+                        + " - " + phoneNumbers[i]);
+            }
+        }
+        System.out.println("Контакты закончились.");
+    }
+
+    //Поиск контакта по имени
+    private void lookForContact() {
+        System.out.println("Имя для поиска: ");
+        String enteredName = sc.nextLine();
+
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] != null && names[i].equalsIgnoreCase(enteredName)) {
+                System.out.println("Номер телефона " + enteredName + ": " + phoneNumbers[i]);
+                return;
+            }
+        }
+        System.out.println("Контакт с именем " + enteredName + " не найден.");
+    }
+
+    //Удаление контакта
+    private void deleteContact() {
+        System.out.println("Введите имя для удаления контакта: ");
+        String nameToDelete = sc.nextLine();
+        boolean flag = false;
+
+        for (int i = 0; i < index; i++) {
+            if (names[i].equalsIgnoreCase(nameToDelete)) {
+                for (int j = i; j <  index - 1; j++) {
+                    names[j] = names[j + 1];
+                    phoneNumbers[j] = phoneNumbers[j + 1];
+                }
+                names[index - 1] = null;
+                phoneNumbers[index - 1] = null;
+                index--;
+                flag = true;
+                System.out.println("Контакт " + nameToDelete + " успешно удален.");
+                break;
+            }
+        }
+        if (!flag) {
+            System.out.println("Контакт с именем " + nameToDelete + " не найден.");
+        }
+    }
+
     //Метод с описанием меню
-    public void textMenu() {
+    private void startMenu() {
         System.out.println("1. Добавить контакт");
         System.out.println("2. Просмотреть контакты");
         System.out.println("3. Найти контакт");
