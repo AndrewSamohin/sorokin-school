e8e7679309e92f96a51931d3fe3fb6a9c1ebe029
Andrew Samohin <andreusamokhin@mail.ru>
2025-08-27 11:32:36 +0300

    Java Core. Merge

--- src/main/java/org/schoolsorokin/javacore/App.java
+++ src/main/java/org/schoolsorokin/javacore/App.java
@@ -1,27 +1,9 @@
 package org.schoolsorokin.javacore;
 
-import org.schoolsorokin.javacore.basics.*;
-
 public class App 
 {
     public static void main( String[] args )
     {
-        /*AnalysisOfRatings analysis = new AnalysisOfRatings();
-        analysis.analysis();
 
-        MovieSelection selection = new MovieSelection();
-        selection.movie();
-
-        HomeworkOnCycles cycles = new HomeworkOnCycles();
-        cycles.cycles();
-
-        HomeworkOnArrays arrays = new HomeworkOnArrays();
-        arrays.arrays();
-
-        WorkingWithStrings strings = new WorkingWithStrings();
-        strings.strings();
-
-        ContactAccountingSystem system = new ContactAccountingSystem();
-        system.accountSystem();*/
     }
 }
